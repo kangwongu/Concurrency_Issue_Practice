230f0a433dd51d644cd52f78b605fb1bf0433a8b
kangwongu <kwg0527@naver.com>
2023-11-01 10:33:14 +0900

    Initial commit

---
+++ src/main/java/com/concurrency_issue_practice/ConcurrencyIssuePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.concurrency_issue_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyIssuePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyIssuePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/concurrency_issue_practice/ConcurrencyIssuePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurrency_issue_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyIssuePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
