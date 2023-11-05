f7371a2b91ac35da3714301229e82c094f07c444
kangwongu <kwg0527@naver.com>
2023-11-05 21:28:10 +0900

    pessimistic lock으로 해결하기

--- src/main/java/com/concurrency_issue_practice/stock/repository/StockRepository.java
+++ src/main/java/com/concurrency_issue_practice/stock/repository/StockRepository.java
@@ -2,6 +2,15 @@ package com.concurrency_issue_practice.stock.repository;
 
 import com.concurrency_issue_practice.stock.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.LockModeType;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s " +
+            "where s.id = :id")
+    Stock getByIdWithPessimisticLock(Long id);
 }
---
+++ src/test/java/com/concurrency_issue_practice/stock/service/PessimisticLockStockServiceTest.java
@@ -0,0 +1,74 @@
+package com.concurrency_issue_practice.stock.service;
+
+import com.concurrency_issue_practice.stock.domain.Stock;
+import com.concurrency_issue_practice.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class PessimisticLockStockServiceTest {
+
+    @Autowired
+    private PessimisticLockStockService pessimisticLockStockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    void before() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 재고감소() {
+        // given
+        // when
+        pessimisticLockStockService.decrease(1L, 1L);
+
+        // then
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertThat(stock.getQuantity()).isEqualTo(99);
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        // given
+        int threadCount = 100;
+        // 멀티스레드, 비동기로 실행하는 작업을 단순화해서 사용할 수 있게 도와줌
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 100개의 요청이 끝날 때까지 기다림
+        CountDownLatch latch = new CountDownLatch(threadCount);     // 다른 쓰레드에서 수행중인 작업이 완료될 때까지 대기할 수 있도록 도와줌
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    pessimisticLockStockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        // then
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertThat(stock.getQuantity()).isEqualTo(0);
+    }
+
+}
