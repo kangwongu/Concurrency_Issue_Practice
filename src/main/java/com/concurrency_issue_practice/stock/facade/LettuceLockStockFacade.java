f4cd9bb11ad5494ecc4e6fbb2493ab657f00aa7e
kangwongu <kwg0527@naver.com>
2023-11-06 11:10:21 +0900

    Redis Lettuce로 해결하기

---
+++ src/main/java/com/concurrency_issue_practice/stock/facade/LettuceLockStockFacade.java
@@ -0,0 +1,27 @@
+package com.concurrency_issue_practice.stock.facade;
+
+import com.concurrency_issue_practice.stock.repository.RedisLockRepository;
+import com.concurrency_issue_practice.stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LettuceLockStockFacade {
+
+    private final RedisLockRepository redisLockRepository;
+    private final StockService stockService;
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(id)) {
+            Thread.sleep(100);
+        }
+
+        try {
+            stockService.decrease(id, quantity);
+        } finally {
+            redisLockRepository.unlock(id);
+        }
+    }
+
+}
